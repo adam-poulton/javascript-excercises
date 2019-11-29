f8b209e4ad41bc40edcb653916d2f318cda6af63
adam-poulton <adam.poulton5@gmail.com>
2019-11-29 14:00:49 +1100

    Completes variousn exercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    let args = [...arguments]
+    let array = args.shift()
+    args.forEach(item => {
+        let index = array.indexOf(item);
+        if (index != -1) array.splice(index, 1);
+    })
+    return array;
 }
 
 module.exports = removeFromArray
