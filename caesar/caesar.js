63d4fb9d2d8cd91078c66d0bf1095aa90eb849fa
adam-poulton <adam.poulton5@gmail.com>
2019-12-01 21:10:40 +1100

    Complete caesar.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function(string, offset) {
+  // wrap negative offsets
+  if (offset < 0)
+    return caesar(string, offset + 26);
 
+  let chypher = "";
+  [...string].forEach(char => {
+    let code = char.charCodeAt(0)
+    if (code >= 65 && code <= 90){
+      // lowercase
+      chypher += String.fromCharCode(((code - 65 + offset) % 26) + 65);
+    } else if(code >= 97 && code <= 122){
+      // uppercase
+      chypher += String.fromCharCode(((code - 97 + offset) % 26) + 97);
+    } else {
+      // other characters
+      chypher += char;
+    }
+  });
+  return chypher;
 }
 
 module.exports = caesar
