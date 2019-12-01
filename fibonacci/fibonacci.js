40ad6e76fba17199235bda9009fd3af57a4e3592
adam-poulton <adam.poulton5@gmail.com>
2019-12-01 21:19:33 +1100

    Complete fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(fn) {
+  return (fn < 0 ? "OOPS" :
+      fn == 0 ? 0 :
+      fn == 1 ? 1 :
+      fibonacci(fn - 1) + fibonacci(fn - 2)
+  )
 }
 
 module.exports = fibonacci
