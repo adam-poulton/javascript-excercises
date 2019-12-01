a07d9b363ace61b756cb9f543930c99da516452e
adam-poulton <adam.poulton5@gmail.com>
2019-12-01 20:31:59 +1100

    Add documentation to palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,6 +1,8 @@
 const palindromes = function(string) {
+  // remove all punctuation and whitespace
   let stripped = string.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
   for (let i = 0; i < Math.floor(stripped.length/2) ; i++) {
+    // compare start and end characters, then move inwards to the middle
     if (stripped[i] !== stripped[stripped.length-i-1]) return false;
   }
   return true;
