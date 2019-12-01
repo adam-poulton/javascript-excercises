f4b5a10b97e73b0fc410c704f6ffb0a0db41c26b
adam-poulton <adam.poulton5@gmail.com>
2019-12-01 22:11:57 +1100

    Complete findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+  const oldest = people.reduce((acc, person) => {
+    const year = (new Date()).getFullYear();
+    const age = person.hasOwnProperty('yearOfDeath') ? person.yearOfDeath - person.yearOfBirth : year - person.yearOfBirth;
+    const maxAge = acc.hasOwnProperty('yearOfDeath') ? acc.yearOfDeath - acc.yearOfBirth : year - acc.yearOfBirth;
+    return age > maxAge ? person : acc
+  }, people[0])
+  return oldest
 }
 
 module.exports = findTheOldest
