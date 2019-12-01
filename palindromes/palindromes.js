bd8b7dd208cede395e8d9626285d9bb9d90d7a3a
adam-poulton <adam.poulton5@gmail.com>
2019-12-01 20:29:43 +1100

    Complete palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+  let stripped = string.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+  for (let i = 0; i < Math.floor(stripped.length/2) ; i++) {
+    if (stripped[i] !== stripped[stripped.length-i-1]) return false;
+  }
+  return true;
 }
 
 module.exports = palindromes
